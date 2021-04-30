53ff207c19223a77d209e5edeedbd6058f77ee63
nathaliaassis <nathaliamassis@gmail.com>
2021-04-29 21:55:50 -0300

    logout constant

--- src/store/constants.js
+++ src/store/constants.js
@@ -1,7 +1,7 @@
 export const LOGIN_REQUEST = "@auth/LOGIN_REQUEST";
 export const LOGIN_SUCCESS = "@auth/LOGIN_SUCCESS";
 export const LOGIN_FAILED = "@auth/LOGIN_FAILED";
-export const LOGOUT = "@auth/LOGIN_FAILED";
+export const LOGOUT = "@auth/LOGOUT";
 
 export const ADD = "ADD";
 export const REMOVE = "REMOVE";
